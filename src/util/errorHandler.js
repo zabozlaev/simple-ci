5a22c4c1e146a4359148f2b6091685fde0ffc051
Ilya <44939563+zabozlaev@users.noreply.github.com>
2020-03-17 14:59:47 +0300

    fix: status

--- src/util/errorHandler.js
+++ src/util/errorHandler.js
@@ -2,7 +2,7 @@ const httpStatusCodes = require("http-status-codes");
 
 module.exports = async (req, reply, error) => {
   const { status = 500, message } = error;
-  reply.status(500);
+  reply.status(status);
   reply.send({
     status,
     message: status == 500 ? httpStatusCodes.getStatusText(status) : message,
